8cc5eacc8c049ed03ea88bcda1fa239daf5b0b77
KURABA SANDEEP <kurabasandeep5@gmail.com>
2025-08-20 13:37:08 +0530

    Add files via upload

---
+++ create .sql
@@ -0,0 +1,18 @@
+create table sandeep (id int, full_name varchar(20),city varchar(20))
+
+insert into sandeep 
+values
+(2,'Yuva','Banglore'),
+(3,'Bhannu','Banglore'),
+(4,'Kumar','Banglore'),
+(5,'Aswini','Banglore'),
+(6,'Manjula','Banglore'),
+(7,'Sigarappa','Banglore')
+
+create table Developers(slno int, name varchar(20),city varchar(20))
+insert into Developers 
+values
+(1,'Sandeep','Banglore'),
+(2,'Abhi','Ongole'),
+(3,'pooji','ongole'),
+(4,'Sriram','india'),
