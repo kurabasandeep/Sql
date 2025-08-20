4ac7ae4138714200e938ba41fab510ccbdb472d4
KURABA SANDEEP <kurabasandeep5@gmail.com>
2025-08-20 13:27:49 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+create table sandeep (id int, full_name varchar(20),city varchar(20))
+
+insert into sandeep 
+values
+(2,'Yuva','Banglore'),
+(3,'Bhannu','Banglore'),
+(4,'Kumar','Banglore'),
+(5,'Aswini','Banglore'),
+(6,'Manjula','Banglore'),
+(7,'Sigarappa','Banglore')
